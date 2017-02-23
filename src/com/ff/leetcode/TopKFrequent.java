eae9fcd9447421bf881ea45de4ae0a98b83dad92
frankfan <frankfancode@gmail.com>
2017-02-23 10:46:26 +0800

    top key max heep

---
+++ src/com/ff/leetcode/TopKFrequent.java
@@ -0,0 +1,23 @@
+package com.ff.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created on 06/02/2017.
+ */
+public class TopKFrequent {
+    public static void main(String[] args) {
+        TopKFrequent t = new TopKFrequent();
+        int[] nums = {1, 1, 1, 2, 2, 2, 3};
+        System.out.println(t.topKFrequent(nums, 3));
+    }
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> topK = new LinkedList<>();
+        return topK;
+
+    }
+
+
+}
