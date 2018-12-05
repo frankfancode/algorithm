49c34f40121ea1dc89c6a408769bb91a6187ca76
frankfan <frankfancode@gmail.com>
2018-12-06 00:25:27 +0800

    [ADD]symmetric treenode

--- src/com/ff/algocasts/AlgoCast4.java
+++ src/com/ff/algocasts/AlgoCast4.java
@@ -1,6 +1,6 @@
 package com.ff.algocasts;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 public class AlgoCast4 {
     public static void main(String[] args) {
@@ -22,7 +22,10 @@ public class AlgoCast4 {
         treeNode.right.left.val = 8;
         treeNode.right.right.val = 4;
 
-        System.out.println();
+        System.out.println(treeNode);
+
+        System.out.println(isSymmetric(treeNode.left, treeNode.right));
+        System.out.println(isSymmetricTreeIterative(treeNode));
     }
 
 
@@ -32,7 +35,38 @@ public class AlgoCast4 {
         TreeNode right;
     }
 
-    private static int[] isSymmetric() {
+    static boolean isSymmetric(TreeNode s, TreeNode t) {
+        if (s != null && t != null)
+            return s.val == t.val &&
+                    isSymmetric(s.left, t.right) && isSymmetric(s.right, t.left);
+        else return s == null && t == null;
+    }
+
+    static boolean isSymmetricTreeIterative(TreeNode rootNode) {
+        if (rootNode == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(rootNode.left);
+        stack.push(rootNode.right);
+        while (!stack.empty()) {
+            TreeNode s = stack.pop();
+            TreeNode t = stack.pop();
+            if (s == null && t == null) continue;
+            if (s == null || t == null) return false;
+            if (s.val != t.val) return false;
+
+            stack.push(s.left);
+            stack.push(t.right);
+
+            stack.push(s.right);
+            stack.push(t.left);
+
+        }
+
+        return true;
 
     }
 
